53842d7bd8206a7984354669d8f0445291da42f4
Felipe Gonzalez <pipegonzalez@Felipe-MacBook-Pro.local>
2024-01-03 18:18:49 -0800

    fetching data from the api using server side

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
 
-const UsersPage = () => {
-  return (
-    <div>
-      Users Page
-    </div>
-  )
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+  };
 }
 
-export default UsersPage
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <strong>Name:</strong> {user.name}
+            <br />
+            <strong>Email:</strong> {user.email}
+            <br />
+            <strong>Address:</strong> {user.address.street}, {user.address.city}
+            ,
+            <small>
+              <i>{user.address.zipcode}</i>
+            </small>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default UsersPage;
